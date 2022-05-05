4208e92cd4148d6d92dcd1897018113d4145437d
rzkfyn <rzkfyn@gmail.com>
2022-05-05 19:56:03 +0700

    scoring

--- script/index.js
+++ script/index.js
@@ -1,74 +1,51 @@
-class Suit{
-    constructor() {
-        this.options = ['kertas', 'gunting', 'batu'];
-        this.winConditions = [
-            ['gunting', 'kertas'],
-            ['kertas', 'batu'],
-            ['batu', 'gunting']
-        ];
-        this.loseConditions = [
-            ['kertas', 'gunting'],
-            ['batu', 'kertas'],
-            ['gunting', 'batu']
-        ];
-    }
-}
-
-class SuitBot extends Suit {
-    constructor() {
-        super();
-    }
-
-    pilihSuit() {
-        return this.options[Math.floor(Math.random() * this.options.length)];
-    }
-}
-
-class User extends Suit {
-    constructor() {
-        super();
-    }
-
-    checkWinStatus(pilihanUser, pilihanBot) {
-        return this.isWin(pilihanUser, pilihanBot) ? 1 : this.isLose(pilihanUser, pilihanBot) ? -1 : 0;
-    }
-
-    isWin(pilihanUser, pilihanBot) {
-        for (const condition of this.winConditions) {
-            if (condition[0] == [pilihanUser, pilihanBot][0] && condition[1] == [pilihanUser, pilihanBot][1]) return true;
-        }
-        return false;
-    }
-
-    isLose(pilihanUser, pilihanBot) {
-        for (const condition of this.loseConditions) {
-            if (condition[0] == [pilihanUser, pilihanBot][0] && condition[1] == [pilihanUser, pilihanBot][1]) return true;
-        }
-        return false;
-    }
-}
-
 const bot = new SuitBot();
 const user = new User();
 
+let userScore = 0;
+let botScore = 0;
+
 document.addEventListener('click', function(ev) {
+    if (!ev.target.classList.contains('pilihansuit')) return;
+
     const pilihanUser = ev.target.dataset.pilihan;
     const pilihanBot = bot.pilihSuit();
+    const intervalId = randomImg();
 
-    if (!ev.target.classList.contains('pilihansuit')) return;
-    let intervalId = randomImg();
+    ev.target.style.filter = 'grayscale(10)';
+
     setTimeout(() => {
         clearInterval(intervalId);
         document.querySelector('section.pilihan-bot img').setAttribute('src', `assets/img/${pilihanBot}.png`);
+        document.querySelector('section.pilihan-bot img').setAttribute('alt', pilihanBot);
+        document.querySelector('section.pilihan-bot img').setAttribute('title', pilihanBot);
         setTimeout(() => {
             user.checkWinStatus(pilihanUser, pilihanBot) == 1 ? alert('Menang!') : user.checkWinStatus(pilihanUser, pilihanBot) == -1 ? alert('Kalah !') : alert('Draw !');
+            user.checkWinStatus(pilihanUser, pilihanBot) == 1 ? plusUserScore() : user.checkWinStatus(pilihanUser, pilihanBot) == -1 ? plusBotScore() : void 0;
+            ev.target.removeAttribute('style');
         }, 300);
-        
     }, 3000);
 });
 
 function randomImg() {
     return setInterval(() => {
-        document.querySelector('section.pilihan-bot img').setAttribute('src', `assets/img/${bot.pilihSuit()}.png`);
+        const pilihan = bot.pilihSuit();
+        document.querySelector('section.pilihan-bot img').setAttribute('src', `assets/img/${pilihan}.png`);
+        document.querySelector('section.pilihan-bot img').setAttribute('alt', pilihan);
+        document.querySelector('section.pilihan-bot img').setAttribute('title', pilihan);
     }, 100);
+}
+
+function plusUserScore() {
+    userScore++;
+    console.log(userScore);
+    setScore(userScore, document.querySelector('.userscore'));
+}
+
+function plusBotScore() {
+    botScore++;
+    setScore(botScore, document.querySelector('.botscore'));
+}
+
+function setScore(score, el) {
+    el.innerText = score;
 }
